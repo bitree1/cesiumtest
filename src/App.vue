b74b3a33c3fa7ab2d8dd0d0e8261d7f19566e289
zoe <1164739892@qq.com>
2020-10-27 10:41:35 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import earth from "./view/earth.vue"
+export default {
+  name: 'App',
+  components : {
+      'earth' : earth
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/view/earth.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div id="cesiumContainer"></div>
+  </div>
+</template>
+
+<script>
+    import { Viewer } from 'cesium/Cesium'
+    import  'cesium/Widgets/widgets.css'
+    export default {
+        name: "earth",
+        data() {
+            return {};
+        },
+        mounted() {
+            let viewer = new Viewer("cesiumContainer");
+        }
+    };
+</script>
+
+<style scoped>
+  .container {
+    width: 100%;
+    height: 100vh;
+  }
+  #cesiumContainer {
+    width: 100%;
+    height: 100vh;
+  }
+</style>
